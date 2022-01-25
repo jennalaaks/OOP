fbfc8247a4b0de92359109bbf18597680966a472
Jenna Laaksovirta <jennalaaksovirt@gmail.com>
2022-01-25 12:48:25 +0200

    Exercises 4 and 5 updated.

---
+++ Exercise 2/tehtava5.py
@@ -0,0 +1,20 @@
+def calc_average(students):
+    values = students.values()
+    average = sum(values) / len(students)
+    return average
+
+def main():
+    # Greate dictionary list
+    students = {}
+
+    # For loop where ask user input
+    for i in range(0, 5):
+        student_name = input("Student name: ")
+
+        grade = int(input("Student grade: "))
+
+        students[student_name] = grade
+
+    print('Students average: ', calc_average(students))
+
+main()
